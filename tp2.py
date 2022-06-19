c60a2396407a8f35d6ecdb1cbb7485316add96dc
Renan <renan.lowe@hotmail.com>
2022-06-19 20:50:01 -0300

    changing method

--- tp2.py
+++ tp2.py
@@ -1,6 +1,4 @@
-from sqlite3 import connect
 import psycopg2     #biblioteca para fazer a conexao com o banco
-from cmath import log
 
 #funcao para criar conexao no banco
 def db_connect():
@@ -11,45 +9,3 @@ def db_connect():
         user='postgres', 
         password='12345')
     return connect
-
-def db_run(sql):
-    connect=db_connect()
-    cursor=connect.cursor()
-    try:
-        cursor.execute(sql)
-        connect.commit()
-        return cursor.fetchall()
-    except(Exception, psycopg2.DatabaseError) as error:
-        connect.rollback()
-        connect.close()
-        return 1
-    cursor.close()
-
-#tabelas do banco
-class Linha:
-    def init(user):
-        user.id=0
-        user.firstColumn=''
-        user.secondColumn=''
-
-    def setId(user, id):
-        user.id=id
-
-    def setFirstColumn(user, firstColumn):
-        user.firstColumn=firstColumn
-
-    def setSecondColumn(user, secondColumn):
-        user.secondColumn=secondColumn
-
-#log - abertura e leitura
-def openLog(file):
-    try:
-        f = open(file, 'r') #abre para leitura
-        print('File open')
-        return f
-    except:
-        print('File error')
-        
-def showLog(f):
-    for line in f:  #printar linha por linha
-        print(line)
