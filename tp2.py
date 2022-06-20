54b28864cd1410e8498cae400471f7c780dcb31e
Renan <renan.lowe@hotmail.com>
2022-06-19 21:42:43 -0300

    create the arrays & make connection

--- tp2.py
+++ tp2.py
@@ -23,3 +23,34 @@ try:
 except:
     print('Erro na abertura do arquivo')
     exit(0)
+
+fileArray=file.read().splitlines()
+
+log=[]
+bd_inicial=[]
+
+for i in fileArray:
+	if(i.startswith("<")):
+		log.append(i)
+	else:
+		bd_inicial.append(i)
+
+numEspacos=0
+for j in bd_inicial:
+	if(j==''):
+		numEspacos+=1	
+for i in range(0,numEspacos,1):
+	bd_inicial.remove('')
+
+connect=db_connect()
+
+bd_vetor=[]
+for line in bd_inicial:
+    splitedLine = line.split('=')
+    for i in range(0,len(splitedLine),1):
+        splitedLine[i]=splitedLine[i].strip()
+        if ',' in splitedLine[i]:
+            splitedLine[i]=splitedLine[i].split(',')
+    splitedLine.append('Nao inserido')
+
+    bd_vetor.append(splitedLine)
